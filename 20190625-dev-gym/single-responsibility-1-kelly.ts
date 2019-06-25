a8cced39621b1952dc220f5eb169f41bd6b420e3
Kelly Williams <kellymarjoriewilliams@gmail.com>
2019-06-25 19:06:22 +1200

    my solution

---
+++ 20190625-dev-gym/single-responsibility-1-kelly.ts
@@ -0,0 +1,62 @@
+// class BookTs {
+//   getTitle(): string {
+//     return "Beauty and the Beast";
+//   }
+
+//   getAuthor(): string {
+//     return "Walt Disney";
+//   }
+
+//   turnPage(): void {
+//     // turns the page
+//   }
+
+//   printCurrentPage(): void {
+//     // prints current page
+//   }
+// }
+
+interface Book {
+  title: string;
+  author: string;
+  pageNumber: number;
+}
+
+class BookInfo {
+  book: Book;
+
+  setTitle(): void {
+    this.book.title = "Beauty and the Beast";
+  }
+  setAuthor(): void {
+    this.book.author = "Walt Disney";
+  }
+
+  getTitle(book: any): string {
+    return book.title;
+  }
+  getAuthor(book: any): string {
+    return book.author;
+  }
+}
+
+class BookReadingFunctions {
+  book: Book;
+
+  pageForward(): void {
+    // page forward
+    this.book.pageNumber += 1;
+  }
+  pageBackward(): void {
+    // page backward
+    this.book.pageNumber -= 1;
+  }
+}
+
+class BookPrint {
+  book: Book;
+
+  printCurrentPage(): number {
+    return this.book.pageNumber;
+  }
+}
